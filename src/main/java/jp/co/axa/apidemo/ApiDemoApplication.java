6d07ff7d10a90d971bc4a9e564b9c3dcaf5ba251
Sunimal Malkaklulage <sskmal@gmail.com>
2024-04-19 07:58:35 +0900

    Application Main
    SpringBoot Main

--- src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
+++ src/main/java/jp/co/axa/apidemo/ApiDemoApplication.java
@@ -1,11 +1,12 @@
 package jp.co.axa.apidemo;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Employees Service", version = "1.0", description = "Employees Management Service"))
 public class ApiDemoApplication {
 
 	public static void main(String[] args) {
